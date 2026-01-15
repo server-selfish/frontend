5e26e6d8ec5626d46a1fc4ee62e0c2ed29ff3d00
daffadon <daffaputranarendra9@gmail.com>
2026-01-15 16:24:33 +0800

    feat(router): integrate tanstack query and enhance router configuration

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,33 @@
-import { createRouter } from '@tanstack/react-router'
-
+import { createRouter } from "@tanstack/react-router";
+import { QueryClient } from "@tanstack/react-query";
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from "./routeTree.gen.ts";
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
+import { DefaultCatchBoundary } from "./components/partial/DefaultCatchBoundary.tsx";
+import { NotFound } from "./components/partial/NotFound.tsx";
 
 // Create a new router instance
 export const getRouter = () => {
+  const queryClient = new QueryClient();
   const router = createRouter({
     routeTree,
-    context: {},
-
+    context: { queryClient },
+    defaultPreload: "intent",
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-  })
+    defaultErrorComponent: DefaultCatchBoundary,
+    defaultNotFoundComponent: () => <NotFound />,
+  });
+  setupRouterSsrQueryIntegration({
+    router,
+    queryClient,
+  });
 
-  return router
+  return router;
+};
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: ReturnType<typeof getRouter>;
+  }
 }
