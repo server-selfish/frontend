2701a016e142ec57d6ce8eae283589c642abf935
daffadon <daffaputranarendra9@gmail.com>
2026-05-05 11:28:12 +0800

    feat(schema): add response schema utils

---
+++ src/schemas/index.ts
@@ -0,0 +1,10 @@
+import { ZodTypeAny, z } from "zod/v3";
+
+export const makeDataSchema = <T extends ZodTypeAny>(dataSchema: T) =>
+  z
+    .object({
+      message: z.string(),
+      data: dataSchema.optional(),
+      error: z.unknown().optional(),
+    })
+    .transform((val) => val.data ?? null);
