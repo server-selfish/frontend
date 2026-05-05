a3af154cd7749edbd694d8bcd0141e900d41b98b
daffadon <daffaputranarendra9@gmail.com>
2026-05-05 12:33:38 +0800

    feat(schema): add user schema

---
+++ src/schemas/user.ts
@@ -0,0 +1,14 @@
+import { z } from "zod/v3";
+import { makeDataSchema } from ".";
+
+const UserInfoSchema = z.object({
+  id: z.string(),
+  provider: z.string(),
+  provider_user_id: z.number(),
+  username: z.string(),
+  email: z.string().nullable().optional(),
+  avatar_url: z.string().nullable().optional(),
+});
+
+export const UserInfoDataResponseSchema = makeDataSchema(UserInfoSchema);
+export type UserInfo = z.infer<typeof UserInfoSchema>;
