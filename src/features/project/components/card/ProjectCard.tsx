1fa0756923df947eb3c53b06149caf3b3e40fab9
daffadon <daffaputranarendra9@gmail.com>
2026-05-07 16:00:05 +0800

    refactor(file): move file to appropriate directory

--- src/features/project/components/card/ProjectCard.tsx
+++ src/features/project/components/card/ProjectCard.tsx
@@ -12,32 +12,48 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import ProjectForm from "./ProjectForm";
+import ProjectForm from "../form/ProjectForm";
 import { Project } from "@/schemas/project";
-import { ReactNode, useState } from "react";
-import { format, parse } from "date-fns";
+import { ReactNode, useMemo, useState } from "react";
+import { dateStringConverter } from "@/lib/date";
+import { cn } from "@/lib/utils";
+import { useSearch } from "@tanstack/react-router";
 interface IProjectCardProps {
   project: Project;
+  className?: string;
 }
-const ProjectCard = ({ project }: IProjectCardProps) => {
-  const cleaned = project.created_at
-    .replace(/([+-]\d{4})\s+\w+$/, "$1")
-    .replace(/\.(\d{3})\d+/, ".$1");
-  const parsed = parse(cleaned, "yyyy-MM-dd HH:mm:ss.SSS xx", new Date());
-  const fca = format(parsed, "yyyy/MM/dd HH:mm:ss");
+const ProjectCard = ({ project, className }: IProjectCardProps) => {
+  const searchParams = useSearch({ from: "/_protected/project/" });
+  const selectedName = useMemo(() => {
+    return searchParams.project;
+  }, [searchParams]);
+  const fca = dateStringConverter(project?.created_at);
   return (
-    <Card className="h-full w-full">
+    <Card
+      className={cn(
+        "h-full w-full duration-150 transition-all",
+        selectedName === project?.name
+          ? "bg-soft-periwinkle border-soft-periwinkle"
+          : "",
+        className
+      )}
+    >
       <CardContent className="flex flex-col gap-1 h-full">
         <div className="flex flex-col justify-between h-full">
           <div className="flex flex-col">
             <CardTitle className="warp-break-words break-all line-clamp-2">
-              {project.name}
+              {project?.name}
             </CardTitle>
-            <CardDescription className="line-clamp-3 mt-1">
-              <p>{project.description}</p>
-            </CardDescription>
+            <p
+              className={cn(
+                "line-clamp-3 mt-1",
+                selectedName === project?.name ? "" : "text-muted-foreground"
+              )}
+            >
+              {project?.description}
+            </p>
           </div>
-          <p className="text-sm text-black">Created at: {fca}</p>
+          <p className="text-sm">Created at: {fca}</p>
         </div>
       </CardContent>
     </Card>
