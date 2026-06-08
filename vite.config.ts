87c637d59ddb0ee72bf71d49445f13f317bd95f3
daffadon <daffaputranarendra9@gmail.com>
2026-06-08 09:28:20 +0800

    chore(config): change deprecated plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import viteTsConfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
 
 const config = defineConfig((config) => ({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     ...(config.command === "build" ? [nitro({ preset: "bun" })] : []),
     devtools(),
     // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
+    // viteTsConfigPaths({
+    //   projects: ["./tsconfig.json"],
+    // }),
+
     tailwindcss(),
     tanstackStart(),
     viteReact(),
