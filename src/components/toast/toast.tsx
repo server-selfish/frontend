5817b3debf3ccc8adf36ae383266051741676770
daffadon <daffaputranarendra9@gmail.com>
2026-06-08 09:33:58 +0800

    refactor(component): handle new line in toast

--- src/components/toast/toast.tsx
+++ src/components/toast/toast.tsx
@@ -1,11 +1,13 @@
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
+import { Fragment } from "react/jsx-runtime";
 
 const toastVariant = cva("text-md", {
   variants: {
     type: {
       info: "text-soft-periwinkle",
       error: "text-red-600",
+      success: "text-green-600",
     },
   },
   defaultVariants: {
@@ -21,7 +23,16 @@ export const Toast = ({ type = "info", message }: ToastProps) => {
   return (
     <div className={cn(toastVariant({ type }))}>
       <p className="font-bold">{type?.toLocaleUpperCase()}</p>
-      <p>{message}</p>
+      <p>
+        {typeof message === "string"
+          ? message.split("\n").map((line, idx) => (
+              <Fragment key={idx}>
+                {line}
+                {idx < message.split("\n").length - 1 && <br />}
+              </Fragment>
+            ))
+          : message}
+      </p>
     </div>
   );
 };
